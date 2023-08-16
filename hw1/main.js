3d206502f7b3894a0b7bd3238c09e67d20dc3820
Anastasiaza333 <anastasiaza05@gmail.com>
2023-08-16 15:38:52 +0300

    first task

---
+++ hw1/main.js
@@ -0,0 +1,15 @@
+let a='hello';
+let b='owu';
+let c ='com';
+let v='ua';
+let w=1;
+let y=10;
+let u=-999;
+let f=123;
+let e=3.14;
+let l=2.7;
+let q=16;
+let t=true;
+let o=false;
+
+console.log(a,b,c,v,w,y,f,e,l,t,o,q,u);
