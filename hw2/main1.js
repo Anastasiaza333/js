68f91762d441765779707185111bae696c41eab7
Anastasiaza333 <anastasiaza05@gmail.com>
2023-08-21 01:56:59 +0300

    commit

---
+++ hw2/main1.js
@@ -0,0 +1,134 @@
+//масиви та об'єкти
+//1
+let arr=[34,69,true,false,1,0,'hey','hello world',3,76];
+console.log(arr);
+//2
+let book1={
+    title:'love' ,
+    pageCount:100,
+    genre:'fantastic'
+
+};
+console.log(book1);
+let book2={
+    title:'criminal' ,
+    pageCount:200,
+    genre:'detective'
+
+};
+console.log(book2);
+let book3={
+    title:'family' ,
+    pageCount:300,
+    genre:'comedy'
+
+};
+console.log(book3);
+//3
+// let book4={
+//     title:'family' ,
+//     pageCount:300,
+//     genre:'comedy',
+//     authors:[name:'vasya',age:34],
+//
+// };
+// console.log(book4);
+// let book5={
+//     title:'family' ,
+//     pageCount:300,
+//     genre:'comedy',
+//     authors:[name:'vasya',age:34],
+//
+// };
+// console.log(book5);
+// let book6={
+//     title:'family' ,
+//     pageCount:300,
+//     genre:'comedy',
+//     authors:[name:'vasya',age:34],
+//
+// };
+// console.log(book6);
+//4
+let users=[
+    {name:'nastiia',username:'light',password:123321},
+    {name:'vasya',username:'boy',password:987765},
+    {name:'nazar',username:'cool',password:643486},
+    {name:'igor',username:'yep',password:121093},
+    {name:'yuliia',username:'water',password:494306},
+    {name:'katiia',username:'sun',password:324143},
+    {name:'sofiia',username:'you',password:898598},
+    {name:'vita',username:'beauty',password:754747},
+    {name:'oliia',username:'flower',password:359095},
+    {name:'sam',username:'kaktus',password:949839}
+
+
+];
+
+console.log(users.password);
+
+//логічні розгалуження
+
+//1
+
+let x=34;
+if(x!==0){
+    console.log('Вірно')
+}else{
+    console.log('Невірно')
+}
+// let a=1;
+// if(a!==0){
+//     console.log('Вірно')
+// }else{
+//     console.log('Невірно')
+// }
+let a=0;
+if(a!==0){
+    console.log('Вірно')
+}else{
+    console.log('Невірно')
+}
+// let a=-3;
+// if(a!==0){
+//     console.log('Вірно')
+// }else{
+//     console.log('Невірно')
+// }
+
+//2
+// let time=
+//3
+//4
+// let schedule=prompt('what day is it today?');
+
+switch(schedule) {
+    case '1':
+        console.log('monday');
+
+
+case '2' :
+    console.log('tuesday');
+
+    case '3' :
+        console.log('wensday');
+
+    case '4' :
+        console.log('thursday');
+
+    case '5' :
+        console.log('friday');
+
+    case '6' :
+        console.log('saturday');
+
+    case '7' :
+        console.log('sunday');
+
+}
+
+//5
+
+//6
+// let x=
+//7
