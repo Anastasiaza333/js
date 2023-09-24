cad8090cef75c547b2cd28d28a8c6d89b7b86c7f
Anastasiaza333 <anastasiaza05@gmail.com>
2023-09-25 01:27:56 +0300

    commit

---
+++ ts/main.js
@@ -0,0 +1,10 @@
+var str;
+str = 'dklkv';
+var numbers = [234, 42];
+;
+var obj;
+obj = {
+    id: 123,
+    name: 'sdkv',
+    skills: ['svvvs', 'vv'],
+};
---
+++ ts/main.ts
@@ -0,0 +1,13 @@
+let str:string;
+str='dklkv';
+let numbers:number[]=[234,42];
+interface IUser
+{id:number;name:string;skills:string[];status?:boolean};
+
+let obj:IUser;
+obj={
+    id:123,
+    name:'sdkv',
+    skills:['svvvs','vv'],
+
+}
